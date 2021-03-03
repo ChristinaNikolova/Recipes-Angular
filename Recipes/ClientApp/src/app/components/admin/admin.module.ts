919155f579096000ebf38fc0d5ac489f16948a49
christinanikolova <christina.nikolova@abv.bg>
2021-03-03 09:57:25 +0200

    create admin's home page

--- Recipes/ClientApp/src/app/components/admin/admin.module.ts
+++ Recipes/ClientApp/src/app/components/admin/admin.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { CategoryCreateComponent } from './category-create/category-create.component';
+import { AdminHomeComponent } from './admin-home/admin-home.component';
 
 
 
 @NgModule({
   declarations: [
+    AdminHomeComponent,
     CategoryCreateComponent
   ],
   imports: [
     CommonModule,
     RouterModule.forChild([
       { path: '', pathMatch: 'full', redirectTo: 'home' },
+      { path: 'administration', component: AdminHomeComponent },
       { path: 'category/create', component: CategoryCreateComponent }
     ])
   ]
